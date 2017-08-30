b78f96edefdc152235dbc625a8cb3e780650458d
PaPaZhao <lizhao15431230@hotmail.com>
2017-08-30 11:58:52 +0800

    last 4a

---
+++ training/test4A/app_cfg.h
@@ -0,0 +1,13 @@
+//
+//  app_cfg.h
+//  training
+//
+//  Created by zhaoli on 2017/8/25.
+//  Copyright © 2017年 zhaoli. All rights reserved.
+//
+
+#ifndef app_cfg_h
+#define app_cfg_h
+
+
+#endif /* app_cfg_h */
